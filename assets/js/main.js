795e76ffea7b85a838a0b97929b6346483be09ca
jikudas <jikudasctgbd@gmail.com>
2022-07-12 14:01:38 +0600

    Qualification Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,26 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'), 
+      tabContent = document.querySelectorAll('[data-content]')
 
+      tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+          const target = document.querySelector(tab.dataset.target)
+
+          tabContent.forEach(tabContents => {
+            tabContents.classList.remove('skills_active')
+          })
+
+          target.classList.add('skills_active')
+
+          tabs.forEach(tab => {
+            tab.classList.remove('skills_active')
+          })
+
+          tab.classList.add('skills_active')
+        })
+      })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
