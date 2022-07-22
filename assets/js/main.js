6024a6582a5a83306104d4bfeca7dc87283bbdf7
jikudas <jikudasctgbd@gmail.com>
2022-07-22 13:07:46 +0600

    Testimonials Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,15 +32,67 @@ const tabs = document.querySelectorAll('[data-target]'),
       })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work_container', {
+  selectors: {
+      target: '.work_card'
+  },
+  animation: {
+      duration: 300
+  }
+});
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll('.work_item')
 
+function activeWork() {
+  linkWork.forEach(l=> l.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click", activeWork))
 
 /*===== Work Popup =====*/
+document.addEventListener("click", (e)=> {
+  if(e.target.classList.contains("work_button")) {
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+})
 
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio_popup-close").addEventListener("click", togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp_thumbnail img").src = portfolioItem.querySelector(".work_img").src;
+  document.querySelector(".portfolio_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;
+  document.querySelector(".portfolio_popup-body").innerHTML = portfolioItem.querySelector(".portfolio_item-details").innerHTML;
+}
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+}) 
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
